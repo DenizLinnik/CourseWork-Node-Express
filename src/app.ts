58eddbbde96a06dec5a7c87466f76ad0440cfb02
DenizLinnik <gabano29042004@gmail.com>
2023-05-10 15:17:10 +0300

    start server

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+const start = async () => {
+  try {
+    app.listen(PORT, () => console.log("Server work " + PORT));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
